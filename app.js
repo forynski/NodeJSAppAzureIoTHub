719507c34a0438b5b6696e166a8b408632892298
Robert Forynski <forynski@gmail.com>
2023-11-25 17:12:28 +0000

    port update

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 const { Client, Message } = require('azure-iot-device');
 
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 8080;
 
 // Azure IoT Hub connection string
 const connectionString = process.env.IOT_HUB_CONNECTION_STRING;
